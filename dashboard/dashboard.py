ba52c545f198fd948c2859f32cc12f8088508efd
7ayoub15 <SAMY82696@GMAIL.COM>
2024-12-26 13:39:38 +0100

    first chart

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 import sqlite3
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 
 #getting the data from the database into a pandas data frame : 
@@ -17,6 +20,7 @@ def get_table_as_dataframe(table_name):
     con.close()
     return df
 
+# get the tables as data frames :
 df_employee = get_table_as_dataframe('Employee')
 df_attendance = get_table_as_dataframe('Attendance')
 df_feedback = get_table_as_dataframe('Feedback')
@@ -25,12 +29,29 @@ df_employee_department = get_table_as_dataframe('EmployeeDepartment')
 #merged the tables into one data frame :
 merged_df = pd.merge(df_employee, df_employee_department, on='employee_id', how='inner')
 merged_df = pd.merge(merged_df, df_attendance[['employee_id','day','time_per_day']], on='employee_id', how='inner')
-merged_df = pd.merge(merged_df, df_attendance[['employee_id','day','time_per_day']], on='employee_id', how='inner')
+merged_df = pd.merge(merged_df, df_feedback, on='employee_id', how='inner')
 
-print(merged_df)
+print(merged_df.columns)
 
-# print(df_employee_department)
-# print(df_attendance)
-# print(df_feedback)
+# # vizualizing data : 
+# def salary_by_departement():
+# # Calculate the average salary by department :
+#     salary_by_departement = merged_df.groupby('department_name')['salary'].mean()
+# # Create the bar plot:
+#     plt.figure(figsize=(10, 6))
+#     sns.barplot(x='department_name', y='salary', data=salary_by_departement.reset_index())
+#     plt.xlabel('Department')
+#     plt.ylabel('Average Salary')
+#     plt.title('Average Salary by Department')
+#     plt.xticks(rotation=45)
+#     # Display the plot using Streamlit
+#     st.header("Average Salary by Department")
+#     st.pyplot(plt)
+ 
+
+# salary_by_departement()
+
+
+
 
 
